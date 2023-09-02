f2c51e5a51c012bd8172bb07c06ca9ba784f49ba
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-02 23:17:04 +0000

    testando outras operações de categoria

--- src/domain/__tests__/category.entity.spec.ts
+++ src/domain/__tests__/category.entity.spec.ts
@@ -40,4 +40,77 @@ describe("Category Unit Tests", () => {
       expect(category.created_at).toBeInstanceOf(Date);
     });
   });
+
+  describe("create command", () => {
+    test("should create a category", () => {
+      const category = Category.create({
+        name: "Movie",
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should create a category with description", () => {
+      const category = Category.create({
+        name: "Movie",
+        description: "some description",
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBe("some description");
+      expect(category.is_active).toBe(true);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+
+    test("should create a category with is_active", () => {
+      const category = Category.create({
+        name: "Movie",
+        is_active: false,
+      });
+      expect(category.category_id).toBeUndefined();
+      expect(category.name).toBe("Movie");
+      expect(category.description).toBeNull();
+      expect(category.is_active).toBe(false);
+      expect(category.created_at).toBeInstanceOf(Date);
+    });
+  });
+
+  //TODO - testes do category_id
+
+  test("should change name", () => {
+    const category = new Category({
+      name: "Movie",
+    });
+    category.changeName("other name");
+    expect(category.name).toBe("other name");
+  });
+
+  test("should change description", () => {
+    const category = new Category({
+      name: "Movie",
+    });
+    category.changeDescription("some description");
+    expect(category.description).toBe("some description");
+  });
+
+  test("should active a category", () => {
+    const category = Category.create({
+      name: "Filmes",
+      is_active: false,
+    });
+    category.activate();
+    expect(category.is_active).toBe(true);
+  });
+
+  test("should disable a category", () => {
+    const category = Category.create({
+      name: "Filmes",
+      is_active: true,
+    });
+    category.deactivate();
+    expect(category.is_active).toBe(false);
+  });
 });
