98262832cb1c44c334870f25e29b3ec6c24c7515
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-03 21:57:27 +0000

    adicionando uuid na entidade categoria

--- src/category/domain/__tests__/category.entity.spec.ts
+++ src/category/domain/__tests__/category.entity.spec.ts
@@ -1,3 +1,4 @@
+import { Uuid } from "../../../shared/domain/value-objects/uuid.vo";
 import { Category } from "../category.entity";
 
 describe("Category Unit Tests", () => {
@@ -6,7 +7,7 @@ describe("Category Unit Tests", () => {
       const category = new Category({
         name: "Movie",
       });
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBeNull();
       expect(category.is_active).toBeTruthy();
@@ -21,7 +22,7 @@ describe("Category Unit Tests", () => {
         is_active: false,
         created_at,
       });
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBe("Movie description");
       expect(category.is_active).toBeFalsy();
@@ -33,7 +34,7 @@ describe("Category Unit Tests", () => {
         description: "Movie description",
       });
 
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBe("Movie description");
       expect(category.is_active).toBeTruthy();
@@ -46,7 +47,7 @@ describe("Category Unit Tests", () => {
       const category = Category.create({
         name: "Movie",
       });
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBeNull();
       expect(category.is_active).toBe(true);
@@ -58,7 +59,7 @@ describe("Category Unit Tests", () => {
         name: "Movie",
         description: "some description",
       });
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBe("some description");
       expect(category.is_active).toBe(true);
@@ -70,7 +71,7 @@ describe("Category Unit Tests", () => {
         name: "Movie",
         is_active: false,
       });
-      expect(category.category_id).toBeUndefined();
+      expect(category.category_id).toBeInstanceOf(Uuid);
       expect(category.name).toBe("Movie");
       expect(category.description).toBeNull();
       expect(category.is_active).toBe(false);
@@ -78,7 +79,23 @@ describe("Category Unit Tests", () => {
     });
   });
 
-  //TODO - testes do category_id
+  describe("category_id field", () => {
+    const arrange = [
+      { category_id: null },
+      { category_id: undefined },
+      { category_id: new Uuid() },
+    ];
+    test.each(arrange)("id = %j", ({ category_id }) => {
+      const category = new Category({
+        name: "Movie",
+        category_id: category_id as any,
+      });
+      expect(category.category_id).toBeInstanceOf(Uuid);
+      if (category_id instanceof Uuid) {
+        expect(category.category_id).toBe(category_id);
+      }
+    });
+  });
 
   test("should change name", () => {
     const category = new Category({
--- src/category/domain/category.entity.ts
+++ src/category/domain/category.entity.ts
@@ -1,5 +1,7 @@
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+
 export type CategoryConstructorProps = {
-  category_id?: string;
+  category_id?: Uuid;
   name: string;
   description?: string | null;
   is_active?: boolean;
@@ -13,14 +15,14 @@ export type CategoryCreateCommand = {
 };
 
 export class Category {
-  category_id: string;
+  category_id: Uuid;
   name: string;
   description: string | null;
   is_active: boolean;
   created_at: Date;
 
   constructor(props: CategoryConstructorProps) {
-    this.category_id = props.category_id;
+    this.category_id = props.category_id ?? new Uuid();
     this.name = props.name;
     this.description = props.description ?? null;
     this.is_active = props.is_active ?? true;
@@ -49,7 +51,7 @@ export class Category {
 
   toJSON() {
     return {
-      category_id: this.category_id,
+      category_id: this.category_id.id,
       name: this.name,
       description: this.description,
       is_active: this.is_active,
