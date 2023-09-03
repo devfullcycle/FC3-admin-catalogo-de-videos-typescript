d3da5df57cada6542de4c7c69125745c8adb2102
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-03 21:29:26 +0000

    criando abstração para objetos de valores

---
+++ src/shared/domain/__tests__/value-object.spec.ts
@@ -0,0 +1,39 @@
+import { ValueObject } from "../value-object";
+
+class StringValueObject extends ValueObject {
+  constructor(readonly value: string) {
+    super();
+  }
+}
+
+class ComplexValueObject extends ValueObject {
+  constructor(readonly prop1: string, readonly prop2: number) {
+    super();
+  }
+}
+
+describe("ValueObject Unit Tests", () => {
+  test("should be equals", () => {
+    const valueObject1 = new StringValueObject("test");
+    const valueObject2 = new StringValueObject("test");
+    expect(valueObject1.equals(valueObject2)).toBe(true);
+
+    const complexValueObject1 = new ComplexValueObject("test", 1);
+    const complexValueObject2 = new ComplexValueObject("test", 1);
+    expect(complexValueObject1.equals(complexValueObject2)).toBe(true);
+  });
+
+  test("should not be equals", () => {
+    const valueObject1 = new StringValueObject("test");
+    const valueObject2 = new StringValueObject("test2");
+    expect(valueObject1.equals(valueObject2)).toBe(false);
+    expect(valueObject1.equals(null as any)).toBe(false);
+    expect(valueObject1.equals(undefined as any)).toBe(false);
+
+    const complexValueObject1 = new ComplexValueObject("test", 1);
+    const complexValueObject2 = new ComplexValueObject("test", 2);
+    expect(complexValueObject1.equals(complexValueObject2)).toBe(false);
+    expect(complexValueObject1.equals(null as any)).toBe(false);
+    expect(complexValueObject2.equals(undefined as any)).toBe(false);
+  });
+});
---
+++ src/shared/domain/value-object.ts
@@ -0,0 +1,15 @@
+import isEqual from "lodash/isEqual";
+
+export abstract class ValueObject {
+  public equals(vo: this): boolean {
+    if (vo === null || vo === undefined) {
+      return false;
+    }
+
+    if (vo.constructor.name !== this.constructor.name) {
+      return false;
+    }
+
+    return isEqual(vo, this);
+  }
+}
