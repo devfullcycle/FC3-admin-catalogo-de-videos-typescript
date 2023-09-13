a50f4ef6810a5949bb27dcd4b8d833cf0df8fdb2
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-13 21:59:32 +0000

    Criando caso de uso de exclusão de categoria

---
+++ src/category/application/delete-category.use-case.ts
@@ -0,0 +1,20 @@
+import { IUseCase } from "../../shared/application/use-case.interface";
+import { Uuid } from "../../shared/domain/value-objects/uuid.vo";
+import { ICategoryRepository } from "../domain/category.repository";
+
+export class DeleteCategoryUseCase
+  implements IUseCase<DeleteCategoryInput, DeleteCategoryOutput>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: DeleteCategoryInput): Promise<DeleteCategoryOutput> {
+    const uuid = new Uuid(input.id);
+    await this.categoryRepo.delete(uuid);
+  }
+}
+
+export type DeleteCategoryInput = {
+  id: string;
+};
+
+type DeleteCategoryOutput = void;
