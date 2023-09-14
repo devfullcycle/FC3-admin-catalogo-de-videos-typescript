11d706a9bb7d64f9c5495e7dd35adbefb3129a27
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-14 08:08:49 +0000

    Testes do caso de uso de exclusão de categoria

---
+++ src/category/application/__tests__/integration/delete-category.use-case.int-spec.ts
@@ -0,0 +1,35 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { setupSequelize } from "../../../../shared/infra/testing/helpers";
+import { Category } from "../../../domain/category.entity";
+import { CategorySequelizeRepository } from "../../../infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "../../../infra/db/sequelize/category.model";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Integration Tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throws error when entity not found", async () => {
+    const uuid = new Uuid();
+    await expect(() => useCase.execute({ id: uuid.id })).rejects.toThrow(
+      new NotFoundError(uuid.id, Category)
+    );
+  });
+
+  it("should delete a category", async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    await useCase.execute({
+      id: category.category_id.id,
+    });
+    await expect(repository.findById(category.category_id)).resolves.toBeNull();
+  });
+});
---
+++ src/category/application/__tests__/unit/delete-category.use-case.spec.ts
@@ -0,0 +1,36 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { InvalidUuidError, Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { CategoryInMemoryRepository } from "../../../infra/db/in-memory/category-in-memory.repository";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Unit Tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throws error when entity not found", async () => {
+    await expect(() =>
+      useCase.execute({ id: "fake id"})
+    ).rejects.toThrow(new InvalidUuidError());
+
+    const uuid = new Uuid();
+
+    await expect(() =>
+      useCase.execute({ id: uuid.id})
+    ).rejects.toThrow(new NotFoundError(uuid.id, Category));
+  });
+
+  it("should delete a category", async () => {
+    const items = [new Category({ name: "test 1" })];
+    repository.items = items;
+    await useCase.execute({
+      id: items[0].category_id.id,
+    });
+    expect(repository.items).toHaveLength(0);
+  });
+});
