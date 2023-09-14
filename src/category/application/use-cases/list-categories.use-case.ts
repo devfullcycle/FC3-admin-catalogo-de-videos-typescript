a1c36c28389ec64f1ddd8b2eaa7965522dedef60
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-14 12:58:28 +0000

    Criando caso de uso de listagem de categorias

---
+++ src/category/application/use-cases/list-categories.use-case.ts
@@ -0,0 +1,43 @@
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from "../../../shared/application/pagination-output";
+import { IUseCase } from "../../../shared/application/use-case.interface";
+import { SortDirection } from "../../../shared/domain/repository/search-params";
+import {
+  CategoryFilter,
+  CategorySearchParams,
+  CategorySearchResult,
+  ICategoryRepository,
+} from "../../domain/category.repository";
+import { CategoryOutput, CategoryOutputMapper } from "./common/category-output";
+
+export class ListCategoriesUseCase
+  implements IUseCase<ListCategoriesInput, ListCategoriesOutput>
+{
+  constructor(private categoryRepo: ICategoryRepository) {}
+
+  async execute(input: ListCategoriesInput): Promise<ListCategoriesOutput> {
+    const params = new CategorySearchParams(input);
+    const searchResult = await this.categoryRepo.search(params);
+    return this.toOutput(searchResult);
+  }
+
+  private toOutput(searchResult: CategorySearchResult): ListCategoriesOutput {
+    const { items: _items } = searchResult;
+    const items = _items.map((i) => {
+      return CategoryOutputMapper.toOutput(i);
+    });
+    return PaginationOutputMapper.toOutput(items, searchResult);
+  }
+}
+
+export type ListCategoriesInput = {
+  page?: number;
+  per_page?: number;
+  sort?: string | null;
+  sort_dir?: SortDirection | null;
+  filter?: CategoryFilter | null;
+};
+
+export type ListCategoriesOutput = PaginationOutput<CategoryOutput>;
---
+++ src/shared/application/pagination-output.ts
@@ -0,0 +1,24 @@
+import { SearchResult } from "../domain/repository/search-result";
+
+export type PaginationOutput<Item = any> = {
+  items: Item[];
+  total: number;
+  current_page: number;
+  last_page: number;
+  per_page: number;
+};
+
+export class PaginationOutputMapper {
+  static toOutput<Item = any>(
+    items: Item[],
+    props: Omit<SearchResult, "items">
+  ): PaginationOutput<Item> {
+    return {
+      items,
+      total: props.total,
+      current_page: props.current_page,
+      last_page: props.last_page,
+      per_page: props.per_page,
+    };
+  }
+}
