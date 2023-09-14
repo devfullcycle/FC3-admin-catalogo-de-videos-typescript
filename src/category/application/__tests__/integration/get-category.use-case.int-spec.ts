0264509961acbf779f2cec15ab2b7e73d2ccbda2
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-14 11:58:29 +0000

    Testes do caso de uso de pegar uma categoria

---
+++ src/category/application/__tests__/integration/get-category.use-case.int-spec.ts
@@ -0,0 +1,39 @@
+import { NotFoundError } from '../../../../shared/domain/errors/not-found.error';
+import { Uuid } from '../../../../shared/domain/value-objects/uuid.vo';
+import { setupSequelize } from '../../../../shared/infra/testing/helpers';
+import { Category } from '../../../domain/category.entity';
+import { CategorySequelizeRepository } from '../../../infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '../../../infra/db/sequelize/category.model';
+import { GetCategoryUseCase } from '../../get-category.use-case';
+
+describe('GetCategoryUseCase Integration Tests', () => {
+  let useCase: GetCategoryUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new GetCategoryUseCase(repository);
+  });
+
+  it('should throws error when entity not found', async () => {
+    const uuid = new Uuid();
+    await expect(() => useCase.execute({ id: uuid.id })).rejects.toThrow(
+      new NotFoundError(uuid.id, Category),
+    );
+  });
+
+  it('should returns a category', async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    const output = await useCase.execute({ id: category.category_id.id });
+    expect(output).toStrictEqual({
+      id: category.category_id.id,
+      name: category.name,
+      description: category.description,
+      is_active: category.is_active,
+      created_at: category.created_at,
+    });
+  });
+});
---
+++ src/category/application/__tests__/unit/get-category.use-case.spec.ts
@@ -0,0 +1,41 @@
+import { NotFoundError } from "../../../../shared/domain/errors/not-found.error";
+import { InvalidUuidError, Uuid } from "../../../../shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { CategoryInMemoryRepository } from "../../../infra/db/in-memory/category-in-memory.repository";
+import { GetCategoryUseCase } from "../../get-category.use-case";
+
+describe("GetCategoryUseCase Unit Tests", () => {
+  let useCase: GetCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new GetCategoryUseCase(repository);
+  });
+
+  it("should throws error when entity not found", async () => {
+    await expect(() => useCase.execute({ id: 'fake id' })).rejects.toThrow(
+        new InvalidUuidError()
+      );
+
+    const uuid = new Uuid();
+    await expect(() => useCase.execute({ id: uuid.id })).rejects.toThrow(
+      new NotFoundError(uuid.id, Category)
+    );
+  });
+
+  it("should returns a category", async () => {
+    const items = [Category.create({ name: "Movie" })];
+    repository.items = items;
+    const spyFindById = jest.spyOn(repository, "findById");
+    const output = await useCase.execute({ id: items[0].category_id.id });
+    expect(spyFindById).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: items[0].category_id.id,
+      name: "Movie",
+      description: null,
+      is_active: true,
+      created_at: items[0].created_at,
+    });
+  });
+});
