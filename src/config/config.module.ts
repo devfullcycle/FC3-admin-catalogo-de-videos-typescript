98178aec912504e331d72b323a48c931a76fb8ec
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-18 21:42:47 +0000

    Validando valores das configurações

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -3,7 +3,42 @@ import {
   ConfigModuleOptions,
   ConfigModule as NestConfigModule,
 } from '@nestjs/config';
+import Joi from 'joi';
 import { join } from 'path';
+
+type DB_SCHEMA_TYPE = {
+  DB_VENDOR: 'mysql' | 'sqlite';
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_DATABASE: string;
+  DB_LOGGING: boolean;
+  DB_AUTO_LOAD_MODELS: boolean;
+};
+
+export const CONFIG_DB_SCHEMA: Joi.StrictSchemaMap<DB_SCHEMA_TYPE> = {
+  DB_VENDOR: Joi.string().required().valid('mysql', 'sqlite'),
+  DB_HOST: Joi.string().required(),
+  DB_DATABASE: Joi.string().when('DB_VENDOR', {
+    is: 'mysql',
+    then: Joi.required(),
+  }),
+  DB_USERNAME: Joi.string().when('DB_VENDOR', {
+    is: 'mysql',
+    then: Joi.required(),
+  }),
+  DB_PASSWORD: Joi.string().when('DB_VENDOR', {
+    is: 'mysql',
+    then: Joi.required(),
+  }),
+  DB_PORT: Joi.number().integer().when('DB_VENDOR', {
+    is: 'mysql',
+    then: Joi.required(),
+  }),
+  DB_LOGGING: Joi.boolean().required(),
+  DB_AUTO_LOAD_MODELS: Joi.boolean().required(),
+};
 
 @Module({})
 export class ConfigModule extends NestConfigModule {
@@ -17,6 +52,9 @@ export class ConfigModule extends NestConfigModule {
         join(process.cwd(), 'envs', `.env.${process.env.NODE_ENV}`),
         join(process.cwd(), 'envs', `.env`),
       ],
+      validationSchema: Joi.object({
+        ...CONFIG_DB_SCHEMA,
+      }),
       ...otherOptions,
     });
   }
