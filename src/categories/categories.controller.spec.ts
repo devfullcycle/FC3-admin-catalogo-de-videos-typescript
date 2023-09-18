2b04698b312db3a0cff7572640fc2d14f8068186
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-18 20:33:29 +0000

    Primeiro teste do controller de Categoria

--- src/categories/categories.controller.spec.ts
+++ src/categories/categories.controller.spec.ts
@@ -1,20 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CategoriesController } from './categories.controller';
-import { CategoriesService } from './categories.service';
+import { CategorySequelizeRepository } from '../core/category/infra/db/sequelize/category-sequelize.repository';
+import { CategoryModel } from '../core/category/infra/db/sequelize/category.model';
+import { SequelizeModule, getModelToken } from '@nestjs/sequelize';
+//piramide de testes
 
 describe('CategoriesController', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        SequelizeModule.forRoot({
+          dialect: 'sqlite' as any,
+          host: ':memory:',
+          logging: false,
+          models: [CategoryModel],
+        }),
+        SequelizeModule.forFeature([CategoryModel]),
+      ],
       controllers: [CategoriesController],
-      providers: [CategoriesService],
+      providers: [
+        {
+          provide: CategorySequelizeRepository,
+          useFactory: (categoryModel: typeof CategoryModel) =>
+            new CategorySequelizeRepository(categoryModel),
+          inject: [getModelToken(CategoryModel)],
+        },
+      ],
     }).compile();
 
     controller = module.get<CategoriesController>(CategoriesController);
   });
 
   it('should be defined', () => {
+    console.log(controller);
     expect(controller).toBeDefined();
   });
 });
