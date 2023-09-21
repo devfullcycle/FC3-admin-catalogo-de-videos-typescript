b745470868ca1e0c2b5fc94a8ab627390182e705
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-21 14:02:14 +0000

    Testes do DatabaseModule

---
+++ src/database/__tests__/database.module.spec.ts
@@ -0,0 +1,77 @@
+import { Test } from '@nestjs/testing';
+import { DatabaseModule } from '../database.module';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
+import { ConfigModule } from '../../config/config.module';
+
+describe('DatabaseModule Unit Tests', () => {
+  describe('sqlite connection', () => {
+    const connOptions = {
+      DB_VENDOR: 'sqlite',
+      DB_HOST: ':memory:',
+      DB_LOGGING: false,
+      DB_AUTO_LOAD_MODELS: true,
+    };
+
+    it('should be a sqlite connection', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connOptions],
+          }),
+        ],
+      }).compile();
+
+      const app = module.createNestApplication();
+      const conn = app.get<Sequelize>(getConnectionToken());
+      expect(conn).toBeDefined();
+      expect(conn.options.dialect).toBe('sqlite');
+      expect(conn.options.host).toBe(':memory:');
+      await conn.close();
+    });
+  });
+
+  describe('mysql connection', () => {
+    const connOptions = {
+      DB_VENDOR: 'mysql',
+      DB_HOST: 'db',
+      DB_DATABASE: 'micro_videos',
+      DB_USERNAME: 'root',
+      DB_PASSWORD: 'root',
+      DB_PORT: 3306,
+      DB_LOGGING: false,
+      DB_AUTO_LOAD_MODELS: true,
+    };
+
+    it('should be a mysql connection', async () => {
+      const module = await Test.createTestingModule({
+        imports: [
+          DatabaseModule,
+          ConfigModule.forRoot({
+            isGlobal: true,
+            ignoreEnvFile: true,
+            ignoreEnvVars: true,
+            validationSchema: null,
+            load: [() => connOptions],
+          }),
+        ],
+      }).compile();
+
+      const app = module.createNestApplication();
+      const conn = app.get<Sequelize>(getConnectionToken());
+      expect(conn).toBeDefined();
+      expect(conn.options.dialect).toBe(connOptions.DB_VENDOR);
+      expect(conn.options.host).toBe(connOptions.DB_HOST);
+      expect(conn.options.database).toBe(connOptions.DB_DATABASE);
+      expect(conn.options.username).toBe(connOptions.DB_USERNAME);
+      expect(conn.options.password).toBe(connOptions.DB_PASSWORD);
+      expect(conn.options.port).toBe(connOptions.DB_PORT);
+      await conn.close();
+    });
+  });
+});
