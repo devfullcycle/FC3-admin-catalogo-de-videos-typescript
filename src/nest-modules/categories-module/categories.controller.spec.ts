288f373ff3d0cfd2f19be1aee2436ae4436a26fc
Luiz Carlos <argentinaluiz@gmail.com>
2023-09-27 17:23:27 +0000

    Testes de unidade no controller de categoria

--- src/nest-modules/categories-module/categories.controller.spec.ts
+++ src/nest-modules/categories-module/categories.controller.spec.ts
@@ -1,22 +1,142 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { SortDirection } from '@core/shared/domain/repository/search-params';
 import { CategoriesController } from './categories.controller';
-import { DatabaseModule } from '../database-module/database.module';
-import { CategoriesModule } from './categories.module';
-import { ConfigModule } from '../config-module/config.module';
-//piramide de testes
+import { CreateCategoryOutput } from '../../core/category/application/use-cases/create-category/create-category.use-case';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import {
+  CategoryCollectionPresenter,
+  CategoryPresenter,
+} from './categories.presenter';
+import { UpdateCategoryOutput } from '../../core/category/application/use-cases/update-category/update-category.use-case';
+import { UpdateCategoryInput } from '../../core/category/application/use-cases/update-category/update-category.input';
+import { GetCategoryOutput } from '../../core/category/application/use-cases/get-category/get-category.use-case';
+import { ListCategoriesOutput } from '../../core/category/application/use-cases/list-categories/list-categories.use-case';
 
-describe('CategoriesController', () => {
+describe('CategoriesController Unit Tests', () => {
   let controller: CategoriesController;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [ConfigModule.forRoot({}), DatabaseModule, CategoriesModule],
-    }).compile();
-
-    controller = module.get<CategoriesController>(CategoriesController);
+    controller = new CategoriesController();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should creates a category', async () => {
+    //Arrange
+    const output: CreateCategoryOutput = {
+      id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockCreateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    //@ts-expect-error defined part of methods
+    controller['createUseCase'] = mockCreateUseCase;
+    const input: CreateCategoryDto = {
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+    };
+
+    //Act
+    const presenter = await controller.create(input);
+
+    //Assert
+    expect(mockCreateUseCase.execute).toHaveBeenCalledWith(input);
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(output));
+  });
+
+  it('should updates a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    const output: UpdateCategoryOutput = {
+      id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockUpdateUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    //@ts-expect-error defined part of methods
+    controller['updateUseCase'] = mockUpdateUseCase;
+    const input: Omit<UpdateCategoryInput, 'id'> = {
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+    };
+    const presenter = await controller.update(id, input);
+    expect(mockUpdateUseCase.execute).toHaveBeenCalledWith({ id, ...input });
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(output));
+  });
+
+  it('should deletes a category', async () => {
+    const expectedOutput = undefined;
+    const mockDeleteUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(expectedOutput)),
+    };
+    //@ts-expect-error defined part of methods
+    controller['deleteUseCase'] = mockDeleteUseCase;
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    expect(controller.remove(id)).toBeInstanceOf(Promise);
+    const output = await controller.remove(id);
+    expect(mockDeleteUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(expectedOutput).toStrictEqual(output);
+  });
+
+  it('should gets a category', async () => {
+    const id = '9366b7dc-2d71-4799-b91c-c64adb205104';
+    const output: GetCategoryOutput = {
+      id,
+      name: 'Movie',
+      description: 'some description',
+      is_active: true,
+      created_at: new Date(),
+    };
+    const mockGetUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    //@ts-expect-error defined part of methods
+    controller['getUseCase'] = mockGetUseCase;
+    const presenter = await controller.findOne(id);
+    expect(mockGetUseCase.execute).toHaveBeenCalledWith({ id });
+    expect(presenter).toBeInstanceOf(CategoryPresenter);
+    expect(presenter).toStrictEqual(new CategoryPresenter(output));
+  });
+
+  it('should list categories', async () => {
+    const output: ListCategoriesOutput = {
+      items: [
+        {
+          id: '9366b7dc-2d71-4799-b91c-c64adb205104',
+          name: 'Movie',
+          description: 'some description',
+          is_active: true,
+          created_at: new Date(),
+        },
+      ],
+      current_page: 1,
+      last_page: 1,
+      per_page: 1,
+      total: 1,
+    };
+    const mockListUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    };
+    //@ts-expect-error defined part of methods
+    controller['listUseCase'] = mockListUseCase;
+    const searchParams = {
+      page: 1,
+      per_page: 2,
+      sort: 'name',
+      sort_dir: 'desc' as SortDirection,
+      filter: 'test',
+    };
+    const presenter = await controller.search(searchParams);
+    expect(presenter).toBeInstanceOf(CategoryCollectionPresenter);
+    expect(mockListUseCase.execute).toHaveBeenCalledWith(searchParams);
+    expect(presenter).toEqual(new CategoryCollectionPresenter(output));
   });
 });
