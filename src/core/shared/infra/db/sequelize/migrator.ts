79aceebc7bb83da5a53533734b9c8da4e1604815
Luiz Carlos <argentinaluiz@gmail.com>
2023-10-11 22:46:22 +0000

    Criando instância do Umzug

---
+++ src/core/shared/infra/db/sequelize/migrator.ts
@@ -0,0 +1,24 @@
+import { join } from 'path';
+import { Sequelize } from 'sequelize';
+import { SequelizeStorage, Umzug, UmzugOptions } from 'umzug';
+
+export function migrator(
+  sequelize: Sequelize,
+  options?: Partial<UmzugOptions>,
+) {
+  return new Umzug({
+    migrations: {
+      glob: [
+        '*/infra/db/sequelize/migrations/*.{js,ts}',
+        {
+          cwd: join(__dirname, '..', '..', '..', '..'),
+          ignore: ['**/*.d.ts', '**/index.ts', '**/index.js'],
+        },
+      ],
+    },
+    context: sequelize,
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    ...(options || {}),
+  });
+}
