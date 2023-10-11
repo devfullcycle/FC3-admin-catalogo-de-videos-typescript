4fe9cef07eaee627c8e9e07c0abf5820712dd880
Luiz Carlos <argentinaluiz@gmail.com>
2023-10-11 21:13:28 +0000

    Configuração para recriar tabelas a cada teste end to end

--- src/nest-modules/shared-module/testing/helpers.ts
+++ src/nest-modules/shared-module/testing/helpers.ts
@@ -2,6 +2,8 @@ import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../../app.module';
 import { applyGlobalConfig } from '../../global-config';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
 
 export function startApp() {
   let _app: INestApplication;
@@ -10,6 +12,9 @@ export function startApp() {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    const sequelize = moduleFixture.get<Sequelize>(getConnectionToken());
+
+    await sequelize.sync({ force: true });
 
     _app = moduleFixture.createNestApplication();
     applyGlobalConfig(_app);
