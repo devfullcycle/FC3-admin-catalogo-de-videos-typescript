4a5177168adc01451dfffec259f8b1ddcf5acb1e
Luiz Carlos <argentinaluiz@gmail.com>
2023-10-11 23:09:54 +0000

    Criando migração pra criar tabela de categorias

---
+++ src/core/category/infra/db/sequelize/migrations/2023.10.11T23.00.02.create-categories-table.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Sequelize } from 'sequelize';
+import type { MigrationFn } from 'umzug';
+
+export const up: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().createTable('categories', {
+    category_id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE(3),
+      allowNull: false,
+    },
+  });
+};
+export const down: MigrationFn<Sequelize> = async ({ context: sequelize }) => {
+  await sequelize.getQueryInterface().dropTable('categories');
+};
