5c3a9d53ad2eaf0a8d5e74bbb84f0c37f448157f
Luiz Carlos <argentinaluiz@gmail.com>
2023-10-26 16:00:17 +0000

    Criando entidade Genre

---
+++ src/core/genre/domain/genre.aggregate.ts
@@ -0,0 +1,94 @@
+import { CategoryId } from '../../category/domain/category.aggregate';
+import { AggregateRoot } from '../../shared/domain/aggregate-root';
+import { Uuid } from '../../shared/domain/value-objects/uuid.vo';
+
+export type GenreConstructorProps = {
+  genre_id?: GenreId;
+  name: string;
+  categories_id: Map<string, CategoryId>;
+  is_active?: boolean;
+  created_at?: Date;
+};
+
+export type GenreCreateCommand = {
+  name: string;
+  categories_id: CategoryId[];
+  is_active?: boolean;
+};
+
+export class GenreId extends Uuid {}
+
+export class Genre extends AggregateRoot {
+  genre_id: GenreId;
+  name: string;
+  categories_id: Map<string, CategoryId>;
+  is_active: boolean;
+  created_at: Date;
+
+  constructor(props: GenreConstructorProps) {
+    super();
+    this.genre_id = props.genre_id ?? new GenreId();
+    this.name = props.name;
+    this.categories_id = props.categories_id;
+    this.is_active = props.is_active ?? true;
+    this.created_at = props.created_at ?? new Date();
+  }
+
+  static create(props: GenreCreateCommand) {
+    const genre = new Genre({
+      ...props,
+      categories_id: new Map(
+        props.categories_id.map((category_id) => [category_id.id, category_id]),
+      ),
+    });
+    //validacao
+    return genre;
+  }
+
+  changeName(name: string) {
+    this.name = name;
+    //validacao
+  }
+
+  addCategoryId(category_id: CategoryId) {
+    this.categories_id.set(category_id.id, category_id);
+  }
+
+  removeCategoryId(category_id: CategoryId) {
+    this.categories_id.delete(category_id.id);
+  }
+
+  syncCategoriesId(categories_id: CategoryId[]) {
+    if (categories_id.length) {
+      throw new Error('Categories id is empty');
+    }
+
+    this.categories_id = new Map(
+      categories_id.map((category_id) => [category_id.id, category_id]),
+    );
+  }
+
+  activate() {
+    this.is_active = true;
+  }
+
+  deactivate() {
+    this.is_active = false;
+  }
+
+  get entity_id() {
+    return this.genre_id;
+  }
+
+  toJSON() {
+    return {
+      genre_id: this.genre_id.id,
+      name: this.name,
+      categories_id: Array.from(this.categories_id.values()).map(
+        (category_id) => category_id.id,
+      ),
+      is_active: this.is_active,
+      created_at: this.created_at,
+    };
+  }
+}
