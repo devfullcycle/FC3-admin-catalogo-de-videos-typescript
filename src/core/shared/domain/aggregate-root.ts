9469df3f62a5638dc67987521a7a26447ad5ddb0
Luiz Carlos <argentinaluiz@gmail.com>
2023-11-23 17:37:15 +0000

    Criando base dos eventos de domínio

--- src/core/shared/domain/aggregate-root.ts
+++ src/core/shared/domain/aggregate-root.ts
@@ -1,3 +1,9 @@
 import { Entity } from './entity';
+import { IDomainEvent } from './events/domain-event.interface';
 
-export abstract class AggregateRoot extends Entity {}
+export abstract class AggregateRoot extends Entity {
+  //vai disparar somente o evento dentro do próprio aggregate
+  applyEvent(event: IDomainEvent) {}
+
+  registerHandler(event: string, handler: (event: IDomainEvent) => void) {}
+}
