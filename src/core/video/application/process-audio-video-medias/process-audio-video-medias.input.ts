4de4a023f058ea16bcda600b3d24a709602089df
Luiz Carlos <argentinaluiz@gmail.com>
2023-12-05 11:34:18 +0000

    Criando caso de uso para terminar processamentos dos vídeos

---
+++ src/core/video/application/process-audio-video-medias/process-audio-video-medias.input.ts
@@ -0,0 +1,47 @@
+import {
+  IsNotEmpty,
+  IsString,
+  IsUUID,
+  IsIn,
+  validateSync,
+} from 'class-validator';
+import { AudioVideoMediaStatus } from '../../../shared/domain/value-objects/audio-video-media.vo';
+
+export type ProcessAudioVideoMediasInputConstructorProps = {
+  video_id: string;
+  encoded_location: string;
+  field: 'trailer' | 'video';
+  status: AudioVideoMediaStatus;
+};
+
+export class ProcessAudioVideoMediasInput {
+  @IsUUID('4')
+  @IsString()
+  @IsNotEmpty()
+  video_id: string;
+
+  @IsNotEmpty()
+  encoded_location: string;
+
+  @IsIn(['trailer', 'video'])
+  @IsNotEmpty()
+  field: 'trailer' | 'video';
+
+  @IsIn([AudioVideoMediaStatus.COMPLETED, AudioVideoMediaStatus.FAILED])
+  @IsNotEmpty()
+  status: AudioVideoMediaStatus;
+
+  constructor(props?: ProcessAudioVideoMediasInputConstructorProps) {
+    if (!props) return;
+    this.video_id = props.video_id;
+    this.encoded_location = props.encoded_location;
+    this.field = props.field;
+    this.status = props.status;
+  }
+}
+
+export class ValidateProcessAudioVideoMediasInput {
+  static validate(input: ProcessAudioVideoMediasInput) {
+    return validateSync(input);
+  }
+}
---
+++ src/core/video/application/process-audio-video-medias/process-audio-video-medias.use-case.ts
@@ -0,0 +1,54 @@
+import { IUseCase } from '../../../shared/application/use-case.interface';
+import { NotFoundError } from '../../../shared/domain/errors/not-found.error';
+import { IUnitOfWork } from '../../../shared/domain/repository/unit-of-work.interface';
+import { AudioVideoMediaStatus } from '../../../shared/domain/value-objects/audio-video-media.vo';
+import { Video, VideoId } from '../../domain/video.aggregate';
+import { IVideoRepository } from '../../domain/video.repository';
+import { ProcessAudioVideoMediasInput } from './process-audio-video-medias.input';
+
+export class ProcessAudioVideoMediasUseCase
+  implements
+    IUseCase<ProcessAudioVideoMediasInput, ProcessAudioVideoMediasOutput>
+{
+  constructor(
+    private uow: IUnitOfWork,
+    private videoRepo: IVideoRepository,
+  ) {}
+
+  async execute(input: ProcessAudioVideoMediasInput) {
+    const videoId = new VideoId(input.video_id);
+    const video = await this.videoRepo.findById(videoId);
+
+    if (!video) {
+      throw new NotFoundError(input.video_id, Video);
+    }
+
+    if (input.field === 'trailer') {
+      if (!video.trailer) {
+        throw new Error('Trailer not found');
+      }
+
+      video.trailer =
+        input.status === AudioVideoMediaStatus.COMPLETED
+          ? video.trailer.complete(input.encoded_location)
+          : video.trailer.fail();
+    }
+
+    if (input.field === 'video') {
+      if (!video.video) {
+        throw new Error('Video not found');
+      }
+
+      video.trailer =
+        input.status === AudioVideoMediaStatus.COMPLETED
+          ? video.video.complete(input.encoded_location)
+          : video.video.fail();
+    }
+
+    this.uow.do(async () => {
+      await this.videoRepo.update(video);
+    });
+  }
+}
+
+type ProcessAudioVideoMediasOutput = void;
