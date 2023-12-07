608d639d5fa3566c6ee8771cc2761644ded829c7
Luiz Carlos <argentinaluiz@gmail.com>
2023-12-07 09:34:26 +0000

    Criando registro provisório do DomainEventMediator

--- src/nest-modules/shared-module/shared.module.ts
+++ src/nest-modules/shared-module/shared.module.ts
@@ -2,6 +2,8 @@ import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { GoogleCloudStorage } from '../../core/shared/infra/storage/google-cloud.storage';
 import { Storage as GoogleCloudStorageSdk } from '@google-cloud/storage';
+import { DomainEventMediator } from '../../core/shared/domain/events/domain-event-mediator';
+import EventEmitter2 from 'eventemitter2';
 
 @Global()
 @Module({
@@ -18,6 +20,11 @@ import { Storage as GoogleCloudStorageSdk } from '@google-cloud/storage';
       },
       inject: [ConfigService],
     },
+    {
+      provide: DomainEventMediator,
+      useValue: new DomainEventMediator(new EventEmitter2()),
+    },
   ],
 })
 export class SharedModule {}
+//modulo de eventos para poder registrar os ouvintes
