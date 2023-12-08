aed7c850118c0bfd4a9a83f5d67db98284ed0bbc
Luiz Carlos <argentinaluiz@gmail.com>
2023-12-08 10:43:38 +0000

    Criando endpoint de atualização de um vídeo

--- src/nest-modules/videos-module/videos.controller.ts
+++ src/nest-modules/videos-module/videos.controller.ts
@@ -8,12 +8,15 @@ import {
   Inject,
   ParseUUIDPipe,
   UploadedFiles,
+  ValidationPipe,
 } from '@nestjs/common';
 import { CreateVideoUseCase } from '../../core/video/application/create-video/create-video.use-case';
 import { UpdateVideoUseCase } from '../../core/video/application/update-video/update-video.use-case';
 import { UploadAudioVideoMediasUseCase } from '../../core/video/application/upload-audio-video-medias/upload-audio-video-medias.use-case';
 import { GetVideoUseCase } from '../../core/video/application/get-video/get-video.use-case';
 import { CreateVideoDto } from './dto/create-video.dto';
+import { UpdateVideoDto } from './dto/update-video.dto';
+import { UpdateVideoInput } from '../../core/video/application/update-video/update-video.input';
 
 @Controller('videos')
 export class VideosController {
@@ -48,11 +51,24 @@ export class VideosController {
   async update(
     @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
     @Body() updateVideoDto: any,
-  ) {}
+  ) {
+    const hasData = Object.keys(updateVideoDto).length > 0;
+
+    if (hasData) {
+      const data = await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(updateVideoDto, {
+        metatype: UpdateVideoDto,
+        type: 'body',
+      });
+      const input = new UpdateVideoInput({ id, ...data });
+      const { id: newId } = await this.updateUseCase.execute(input);
+      return await this.getUseCase.execute({ id: newId });
+    }
+  }
 
   @Patch(':id/upload')
   uploadFile(
-    @UploadedFiles()
     @Body()
     data,
   ) {}
