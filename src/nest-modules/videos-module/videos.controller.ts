538f0594e39f1e9407c8b530b39b96da93887e47
Luiz Carlos <argentinaluiz@gmail.com>
2023-12-09 09:32:45 +0000

    Criando endpoint para o upload dos arquivos

--- src/nest-modules/videos-module/videos.controller.ts
+++ src/nest-modules/videos-module/videos.controller.ts
@@ -9,6 +9,8 @@ import {
   ParseUUIDPipe,
   UploadedFiles,
   ValidationPipe,
+  UseInterceptors,
+  BadRequestException,
 } from '@nestjs/common';
 import { CreateVideoUseCase } from '../../core/video/application/create-video/create-video.use-case';
 import { UpdateVideoUseCase } from '../../core/video/application/update-video/update-video.use-case';
@@ -17,6 +19,8 @@ import { GetVideoUseCase } from '../../core/video/application/get-video/get-vide
 import { CreateVideoDto } from './dto/create-video.dto';
 import { UpdateVideoDto } from './dto/update-video.dto';
 import { UpdateVideoInput } from '../../core/video/application/update-video/update-video.input';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { UploadAudioVideoMediaInput } from '../../core/video/application/upload-audio-video-medias/upload-audio-video-media.input';
 
 @Controller('videos')
 export class VideosController {
@@ -47,12 +51,34 @@ export class VideosController {
     return await this.getUseCase.execute({ id });
   }
 
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'banner', maxCount: 1 },
+      { name: 'thumbnail', maxCount: 1 },
+      { name: 'thumbnail_half', maxCount: 1 },
+      { name: 'trailer', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+    ]),
+  )
   @Patch(':id')
   async update(
     @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
     @Body() updateVideoDto: any,
+    @UploadedFiles()
+    files: {
+      banner?: Express.Multer.File[];
+      thumbnail?: Express.Multer.File[];
+      thumbnail_half?: Express.Multer.File[];
+      trailer?: Express.Multer.File[];
+      video?: Express.Multer.File[];
+    },
   ) {
+    const hasFiles = files ? Object.keys(files).length : false;
     const hasData = Object.keys(updateVideoDto).length > 0;
+
+    if (hasFiles && hasData) {
+      throw new BadRequestException('Files and data cannot be sent together');
+    }
 
     if (hasData) {
       const data = await new ValidationPipe({
@@ -62,14 +88,99 @@ export class VideosController {
         type: 'body',
       });
       const input = new UpdateVideoInput({ id, ...data });
-      const { id: newId } = await this.updateUseCase.execute(input);
-      return await this.getUseCase.execute({ id: newId });
+      await this.updateUseCase.execute(input);
     }
+
+    const hasMoreThanOneFile = Object.keys(files).length > 1;
+
+    if (hasMoreThanOneFile) {
+      throw new BadRequestException('Only one file can be sent');
+    }
+
+    const hasAudioVideoMedia = files.trailer?.length || files.video?.length;
+    const fieldField = Object.keys(files)[0];
+    const file = files[fieldField][0];
+
+    if (hasAudioVideoMedia) {
+      const dto: UploadAudioVideoMediaInput = {
+        video_id: id,
+        field: fieldField as any,
+        file: {
+          raw_name: file.originalname,
+          data: file.buffer,
+          mime_type: file.mimetype,
+          size: file.size,
+        },
+      };
+
+      const input = await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(dto, {
+        metatype: UploadAudioVideoMediaInput,
+        type: 'body',
+      });
+
+      await this.uploadAudioVideoMedia.execute(input);
+    } else {
+      //use case upload image media
+    }
+    return await this.getUseCase.execute({ id });
   }
 
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'banner', maxCount: 1 },
+      { name: 'thumbnail', maxCount: 1 },
+      { name: 'thumbnail_half', maxCount: 1 },
+      { name: 'trailer', maxCount: 1 },
+      { name: 'video', maxCount: 1 },
+    ]),
+  )
   @Patch(':id/upload')
-  uploadFile(
-    @Body()
-    data,
-  ) {}
+  async uploadFile(
+    @Param('id', new ParseUUIDPipe({ errorHttpStatusCode: 422 })) id: string,
+    @UploadedFiles()
+    files: {
+      banner?: Express.Multer.File[];
+      thumbnail?: Express.Multer.File[];
+      thumbnail_half?: Express.Multer.File[];
+      trailer?: Express.Multer.File[];
+      video?: Express.Multer.File[];
+    },
+  ) {
+    const hasMoreThanOneFile = Object.keys(files).length > 1;
+
+    if (hasMoreThanOneFile) {
+      throw new BadRequestException('Only one file can be sent');
+    }
+
+    const hasAudioVideoMedia = files.trailer?.length || files.video?.length;
+    const fieldField = Object.keys(files)[0];
+    const file = files[fieldField][0];
+
+    if (hasAudioVideoMedia) {
+      const dto: UploadAudioVideoMediaInput = {
+        video_id: id,
+        field: fieldField as any,
+        file: {
+          raw_name: file.originalname,
+          data: file.buffer,
+          mime_type: file.mimetype,
+          size: file.size,
+        },
+      };
+
+      const input = await new ValidationPipe({
+        errorHttpStatusCode: 422,
+      }).transform(dto, {
+        metatype: UploadAudioVideoMediaInput,
+        type: 'body',
+      });
+
+      await this.uploadAudioVideoMedia.execute(input);
+    } else {
+      //use case upload image media
+    }
+    return await this.getUseCase.execute({ id });
+  }
 }
